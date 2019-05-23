d05c4badd88c6a51bc3a58f3e18f80a8061f633d
hogostan <wluhan@foxmail.com>
2019-05-23 14:39:44 +0800

    Registration Accept msg (_5GSTrackingAreaIdentityList IE) encode/decode

--- src/amf/NasLibrary/ies/_5GSTrackingAreaIdentityList.h
+++ src/amf/NasLibrary/ies/_5GSTrackingAreaIdentityList.h
@@ -7,9 +7,34 @@
 #define _5GS_TRACKING_AREA_IDENTITY_LIST_MINIMUM_LENGTH 9
 #define _5GS_TRACKING_AREA_IDENTITY_LIST_MAXIMUM_LENGTH 114
 
-typedef struct {
-}PartialTrackingAreaIdentityList;
-typedef bstring _5GSTrackingAreaIdentityList;
+#define LIST_OF_TACS_BELONGING_TO_ONE_PLMN_WITH_NON_CONSECUTIVE_TAC_VALUES 0b00
+#define LIST_OF_TACS_BELONGING_TO_ONE_PLMN_WITH_CONSECUTIVE_TAC_VALUES 0b01
+#define LIST_OF_TAIS_BELONGING_TO_DIFFERENT_PLMNS 0b10
+
+struct MccMnc{
+  uint16_t mcc;
+  uint16_t mnc;
+  struct MccMnc * next;
+};
+
+struct TrackingAreaIdentity{
+  uint8_t tac;
+  uint16_t tacContinued;
+  struct TrackingAreaIdentity * next;
+};
+
+struct PartialTrackingAreaIdentityList{
+  uint8_t typeOfList;
+  uint8_t numberOfElements;
+  struct MccMnc * mcc_mnc;
+  struct TrackingAreaIdentity *tai;
+  struct PartialTrackingAreaIdentityList * next;
+};
+
+typedef struct{
+  int listSize;
+  struct PartialTrackingAreaIdentityList * partialTrackingAreaIdentityList;
+} _5GSTrackingAreaIdentityList;
 
 int encode__5gs_tracking_area_identity_list ( _5GSTrackingAreaIdentityList _5gstrackingareaidentitylist, uint8_t iei, uint8_t * buffer, uint32_t len  ) ;
 int decode__5gs_tracking_area_identity_list ( _5GSTrackingAreaIdentityList * _5gstrackingareaidentitylist, uint8_t iei, uint8_t * buffer, uint32_t len  ) ;
